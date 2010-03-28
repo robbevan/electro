2e24c69587718011454af9a85a3f0fabfd279547
Rob Bevan <robbevan@xpt.com>
2010-03-28 13:54:55 +0100

    render template if one is found instead of Page

--- app/controllers/electro/pages_controller.rb
+++ app/controllers/electro/pages_controller.rb
@@ -2,9 +2,19 @@ class Electro::PagesController < ApplicationController
   unloadable
   # GET /pages/:id
   def show
+    template = "electro/pages/#{params[:name]}"
+    render(:template => "electro/pages/#{params[:name]}") and return if template_exists?(template)
     @page = Page.find_by_slug((params[:name] ? params[:name].first : params[:id]).to_s.downcase)
     if (@content = (@page.content rescue {})).blank?
       render(:file => "#{RAILS_ROOT}/public/404.html", :status => 404, :layout => false) and return
     end
   end
+  
+  protected
+  
+    def template_exists?(path)
+      view_paths.find_template(path, response.template.template_format)
+    rescue ActionView::MissingTemplate
+      false
+    end
 end
